d9f61e99c7040d6502425243f46ba2324113f602
porozera <107874967+porozera@users.noreply.github.com>
2022-11-08 23:03:12 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,85 @@
 import java.util.Scanner;
 
 public class Main{
-public int pilihanUser;
+
     public static void main(String[] args){
-        
+        int pilihanUser;
+        String komputer;
+        String pilihan;
+        String[] element = {"Manusia","Zombie", "Robot"};
         System.out.println("=================================");
         System.out.println("APOCALYPSE DAY GAME ");
         System.out.println(" 1.MANUSIA \n 2.ZOMBIE \n 3.ROBOT ");
         System.out.println("=================================");
 
-        try (Scanner input = new Scanner(System.in)) {
-            System.out.print("Masukkan Nomor Jagoan Anda: ");
-            GameOOP.anda(input.nextInt());
+        Scanner input = new Scanner(System.in); 
+        System.out.print("Masukkan Nomor Jagoan Anda: ");
+        pilihanUser = input.nextInt();
+
+
+        if(pilihanUser == 1){
+            pilihan = element[0];
+            System.out.println("Pilihan anda : "+ pilihan);
+            }
+        else if(pilihanUser == 2){
+            pilihan = element[1];
+            System.out.println("Pilihan anda : "+ pilihan);
+            }
+        else if(pilihanUser == 3){
+            pilihan = element[2];
+            System.out.println("Pilihan anda : "+ pilihan);
+            }
+        else {
+            System.out.println("Tidak ada pilihan");
         }
-    
+            
+
+
         int generate = (int) (Math.random()*3+1);
-        if(generate==1);
+
+        if(generate==1){
+            komputer = "Manusia";
+            System.out.println("Pilihan Komputer : " + komputer);
+        }
+        else if(generate==2){
+            komputer = "Zombie";
+            System.out.println("Pilihan Komputer : " + komputer);
+        }
+        else{
+            komputer = "Robot";
+            System.out.println("Pilihan Komputer : " + komputer);
+        }
 
 
-     
+
+
+        if (pilihanUser == 1 && komputer == "Manusia"){
+            System.out.println("Hasil : Seri");
+        }
+        else if (pilihanUser == 1 && komputer == "Zombie" ){
+            System.out.println("Hasil : Kalah");
+        }
+        else if (pilihanUser == 1 && komputer == "Robot"){
+            System.out.println("Hasil : Menang");
+        }
+        else if (pilihanUser == 2 && komputer == "Manusia"){
+            System.out.println("Hasil : Menang");
+        }
+        else if (pilihanUser == 2 && komputer == "Zombie"){
+            System.out.println("Hasil : Seri");
+        }
+        else if (pilihanUser == 2 && komputer == "Robot"){
+            System.out.println("Hasil : Kalah");
+        }
+        else if (pilihanUser == 3 && komputer == "Manusia"){
+            System.out.println("Hasil : Kalah");
+        }
+        else if (pilihanUser == 3 && komputer == "Zombie"){
+            System.out.println("Hasil : Menang");
+        }
+        else if (pilihanUser == 3 && komputer == "Robot"){
+            System.out.println("Hasil : Seri");
+        }
+
     }
 }
